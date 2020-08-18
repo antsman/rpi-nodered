d22b4f237af9a06fac7efcf86719cb72aa1af004
Aivo Antsman <antsman@users.noreply.github.com>
2020-08-18 13:36:07 +0300

    Dev docker custom (#1)
    * Use node-red-docker/docker-custom
    * Set ARCH=arm
    * Adjust image tag
    * Fix NODERED_VERSION, drop newline
    * Set OS=buster
    * Use apt-get with Debian
    * Hide packages not found
    * Adjust adduser
    * Hide apk's --virtual buildtools
    * Add debian net-tools (netstat)
    * Add apt-get update
    * Add lirc, try pushing

--- get-versions.sh
+++ get-versions.sh
@@ -3,7 +3,7 @@
 VERSIONS=version.properties
 
 test -n "$1" && (
-  echo NODERED_VERSION=\"$(docker exec -t $1 cat /usr/src/node-red/package.json | grep version | awk '{ print $2 }' | tr -d '",')\" | tee -a $VERSIONS
+  echo NODERED_VERSION=\"$(docker exec -t $1 cat /usr/src/node-red/package.json | grep version | awk '{ print $2 }' | tr -d '",\r')\" | tee $VERSIONS
 
   OS_ID=$(docker exec -t $1 cat /etc/os-release | grep -G ^ID= | awk -F= '{ print $2 }' | tr -d '\r')
   OS_VERSION_ID=$(docker exec -t $1 cat /etc/os-release | grep -G VERSION_ID= | awk -F= '{ print $2 }' | tr -d '"\r')
